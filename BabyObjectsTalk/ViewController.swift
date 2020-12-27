04f3d75a1919a92a65ebef4e9183beb8844dd331
Glenn Posadas <hello@glennvon.com>
2020-12-27 19:28:35 +0800

    add fresh project.

---
+++ BabyObjectsTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BabyObjectsTalk
+//
+//  Created by Glenn Posadas on 12/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
